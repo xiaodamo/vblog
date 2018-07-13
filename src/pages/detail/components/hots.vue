d08372122aa07ee99673dce59f6edcaf45c15630
“wangzhiyou <wangzhiyou@ihmair.cn>
2018-07-13 13:22:54 +0800

    modify zhe first hot article's detail

--- src/pages/detail/components/hots.vue
+++ src/pages/detail/components/hots.vue
@@ -34,16 +34,43 @@ export default {
 			let arr = []
 			arr = this.list.slice(0,1)
 			data = arr[0]
-			data.content = (data.content).replace(/<[^>]+>/g,"")
+			data.content = this.subString((data.content).replace(/<[^>]+>/g,""), 100, true)
 		  }
 
 		  return data
 		  
-	  },
+		},
 	  otherHot(){
-		return this.list.slice(1)
+			return this.list.slice(1)
 	  }
-  }
+	},
+	methods:{
+		subString(str, len, hasDot) {
+            let newLength = 0
+            let newStr = ""
+            let chineseRegex = /[^\x00-\xff]/g
+            let singleChar = ""
+            let strLength = str.replace(chineseRegex, "**").length
+            for (let i = 0; i < strLength; i++) {
+                singleChar = str.charAt(i).toString()
+                if (singleChar.match(chineseRegex) != null) {
+                    newLength += 2
+                }
+                else {
+                    newLength++
+                }
+                if (newLength > len) {
+                    break
+                }
+                newStr += singleChar
+            }
+
+            if (hasDot && strLength > len) {
+                newStr += "..."
+            }
+            return newStr
+		},
+	}
 };
 </script>
 
